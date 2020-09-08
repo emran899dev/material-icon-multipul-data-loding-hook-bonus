178093ec867bc504503d6fdd82e29147f7a7cb70
emran899dev <mdemrannazir9999@gmail.com>
2020-09-08 09:21:29 -0600

    material-ui icon using

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 
 function App() {
+  const [likeColor,setLikeColor] = useState('')
+  const hendelLike = () => {
+    const color = likeColor ? '' : 'primary'
+    setLikeColor(color)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* materila-ui-icon */}
+      <ThumbUpAltIcon onClick={hendelLike} color={likeColor}/>
     </div>
   );
 }
